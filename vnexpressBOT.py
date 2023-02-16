1b936cd1072d0077c81e16fca1423e5cd0fd9839
Quang Hùng Đỗ <doquanghung25102001@gmail.com>
2023-02-16 08:03:00 +0700

    ok

--- vnexpressBOT.py
+++ vnexpressBOT.py
@@ -6,25 +6,25 @@ import sys
 import News
 # telegram
 import logging
-# from telegram import __version__ as TG_VER
+from telegram import __version__ as TG_VER
 from telegram import  Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 baseUrl = "https://vnexpress.net/"  
-TOKEN = '6018262692:AAFZU5Pm05WeNlO5xRhhO15-yujLnLYaxmg'
+TOKEN = ''
 #////////////////////$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$========================================
 
 print("Bot Starting....")
-# try:
-#     from telegram import __version_info__
-# except ImportError:
-#     __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
-# if __version_info__ < (20, 0, 0, "alpha", 1):
-#     raise RuntimeError(
-#         f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-#         f"{TG_VER} version of this example, "
-#         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-#     )
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
 
 # Enable logging
 logging.basicConfig(
